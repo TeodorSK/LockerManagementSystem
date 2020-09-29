884368a7139cd80ee922ada6654e01960cd52bfd
TeoPC <sandel.konjevic.teodor@gmail.com>
2020-09-29 18:33:53 -0400

    student db check

--- lms1.0/webapp.rkt
+++ lms1.0/webapp.rkt
@@ -38,8 +38,9 @@
   (if (or (member "instructor" activeclasses) (member "staff" activeclasses))
             
       (if (is-admin? (extract-binding/single 'cas-employeenumber (request-headers request)) (open-input-file (build-path files-path "auth_admins")))
-          ((set! admin-firstname (extract-binding/single 'cas-firstname (request-headers request)))     
-           (student-unauth-page request))
+          ((admin-unauth-page request)
+           (set! admin-firstname (extract-binding/single 'cas-firstname (request-headers request)))     
+           )
           (admin-unauth-page request))
             
        (student-start request))
